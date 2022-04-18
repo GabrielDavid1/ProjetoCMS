07c13c21faede9e9b0a75b298e12cba860f961d8
GabrielDavid1 <biocamendeschico@gmail.com>
2022-04-18 19:10:04 -0300

    Add files via upload

---
+++ components/Topicos/MenuDeContexto/MenuContexto.tsx
@@ -0,0 +1,76 @@
+/* Contexto */
+import { useList } from '../../../contexts/useTopicos';
+import { useModal } from '../../../contexts/useModal';
+
+/* Menu de Contexto e icones */
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+import {FaPen,FaRegWindowRestore} from 'react-icons/fa'
+import {RiDeleteBin6Line} from 'react-icons/ri'
+
+interface tipagemProps {
+    grupo: boolean;
+}
+
+interface RenderTree {
+    id: string;
+    name: string;
+    children: RenderTree[];
+  }
+
+export const MenuContexto = (( {grupo} : tipagemProps) => {
+    /* Variaveis */
+    const {
+        idTotal, 
+        adicionaGrupo, copiaGrupo,
+        setSelected, setAdicionaGrupo,
+        expanded, setExpanded
+    } = useList();
+
+    const { handleOpen, handleOpen2 } = useModal();
+
+    /* impede ações nos elementos filhos do grupo escolhido */
+    const filhosDoGrupo = ( nodes:RenderTree ) => {
+        nodes.children.map((node) => { 
+        if(node.children.length > 0) {
+            let index = expanded.findIndex(elemento => elemento === node.id);
+
+            if(index !== -1 && node.id !== idTotal){
+            expanded.splice(index, 1);
+            setExpanded({...expanded}); 
+            }
+        }
+        });
+    }
+
+    const acionarToggles = () => {
+        let copiaExpanded = expanded;
+        setSelected([idTotal]);
+        setAdicionaGrupo(!adicionaGrupo);
+        filhosDoGrupo(copiaGrupo[0]);
+        setExpanded(copiaExpanded);
+    }
+
+    /*********************  Render *************************/
+    return (
+      <> 
+        <ContextMenu id="contextmenu">
+          {grupo && 
+            <MenuItem onClick={() => acionarToggles()}>
+              <FaRegWindowRestore className="addgrupo"/>
+              <span> {adicionaGrupo ? 'Parar de adicionar' :  'Adicionar no Grupo' } </span>
+            </MenuItem> 
+          } 
+
+          <MenuItem onClick={handleOpen}>
+            <FaPen className="renomear"/>
+            <span>Renomear</span>
+          </MenuItem>
+
+          <MenuItem onClick={handleOpen2}>
+            <RiDeleteBin6Line className="remover"/>
+            <span>Remover</span>
+          </MenuItem>
+        </ContextMenu>
+      </>
+    );
+});
