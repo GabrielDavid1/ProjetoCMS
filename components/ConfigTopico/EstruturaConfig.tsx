0c3e11c472418fbc227e5080cf791a780ff0eeca
GabrielDavid1 <biocamendeschico@gmail.com>
2022-04-18 19:05:53 -0300

    Add files via upload

---
+++ components/ConfigTopico/EstruturaConfig.tsx
@@ -0,0 +1,10 @@
+import { Retangulo } from "./Retangulo/Retangulo";
+
+export function EstruturaConfig () {
+    return (
+      <div className="conteudo">
+          <h2> Configurações </h2>
+          <Retangulo />
+      </div>
+   )
+}
